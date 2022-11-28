b610aec3106ffa15416e471b26cd05ad1238cb70
Jakub Woźny <jakub2787@gmail.com>
2022-11-28 15:55:39 +0100

    added submit to form and value to select

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,32 +1,40 @@
 import "./style.css"
 const Form = () => {
+  const currency = [
+    {name:"EUR",value:"4,68"},
+    {name:"USD",value:"4,49"},
+    {name:"NOK",value:"0,45"}
+  ]
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+  }
     return (
-        <form className="form js-form">
+        <form onSubmit={onFormSubmit} className="form">
         <fieldset className="form__fieldset">
           <legend className="form__legend">Kalkulator walut</legend>
           <label className="form__label">
             <span className="form__labelText">
               Kwota do przeliczenia:
             </span>
-            <input className="form__input js-amount" type="number" required min="1" step="1" />
+            <input className="form__input " type="number" required min="1" step="1" />
           </label>
           <p>
             <label className="form__label">
               <span className="form__labelText">
                 Waluta:
               </span>
-              <select className="form__input js-currency" name="Select a currency">
-                <option value="EUR">EUR</option>
-                <option value="USD">USD</option>
-                <option value="NOK">NOK</option>
+              <select value={currency} className="form__input" name="Select a currency">
+                <option>EUR</option>
+                <option>USD</option>
+                <option>NOK</option>
               </select>
             </label>
           </p>
-          <button className="form__button js-form__button">Przelicz</button>
+          <button className="form__button ">Przelicz</button>
           <p>
             <label className="form__label">
               Otrzymasz:
-              <strong className="js-result"></strong>
+              <strong className="result"></strong>
             </label>
           </p>
         </fieldset>

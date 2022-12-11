6896bf49b174ca02e653a5efe9db2a9328ba4fb3
Jakub Woźny <jakub2787@gmail.com>
2022-12-11 14:34:00 +0100

    refactoring functions

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -3,24 +3,28 @@ import "./style.css";
 
 const Clock = () => {
     const [date, setDate] = useState(new Date());
+
+    const formatedDate = date.toLocaleString("pl",  {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric"
+    });
+
     useEffect(() => {
-        const timerId = setInterval(setDate(new Date()), 1000)
+        const timerId = setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+
         return () => {
             clearInterval(timerId)
         }
-    }, [date]);
+    }, []);
 
     return (
-        <span className="clock">Dzisiaj jest {date.toLocaleString
-            ("pl",
-                {
-                    weekday: "long",
-                    day: "numeric",
-                    month: "long",
-                    hour: "numeric",
-                    minute: "numeric",
-                    second: "numeric"
-                })}
+        <span className="clock">Dzisiaj jest {formatedDate}
         </span>
     )
 }

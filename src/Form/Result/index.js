e9a13ec6a616c23371b49d78aa72c19564d437ce
Jakub Woźny <jakub2787@gmail.com>
2022-12-23 02:07:20 +0100

    removed Result  to Form

---
+++ src/Form/Result/index.js
@@ -0,0 +1,16 @@
+import { StyledResult } from "./styled";
+
+const Result = ({ result }) => (
+    <StyledResult>
+        {result && (
+            <>
+                {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=&nbsp;
+                <strong>
+                    {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
+                </strong>
+            </>
+        )}
+    </StyledResult>
+);
+
+export default Result;
--- src/Form/Result/styled.js
+++ src/Form/Result/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const StyledResult = styled.p`
+export const StyledResult = styled.span`
     display: flex;
     justify-content: center;
     font-size: 19px;

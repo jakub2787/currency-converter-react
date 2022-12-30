81626b22d8addd35f5ee94f3d109f79e5e957193
Jakub Woźny <jakub2787@gmail.com>
2022-12-30 16:51:13 +0100

    added loading spinner

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,21 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Result from "./Result";
-import { Button, Fieldset, Header, StyledForm, Input, Label, LabelText, Select } from "./styled";
-import { useDate, useRatesData} from "./useRatesData";
+import { Button, Fieldset, Header, Input, Label, LabelText, Select } from "./styled";
+import { useRatesData } from "./useRatesData";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const Form = () => {
-  
+
   const [amount, setAmount] = useState("");
   const [currency, setCurrency] = useState("EUR");
   const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const currenciesData = useRatesData();
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000)
+
+  }, []);
 
   const onSelectCurrency = ({ target }) => setCurrency(target.value);
   const onSelectChange = ({ target }) => setAmount(target.value);
 
+  const currenciesData = useRatesData();
+
   const calculateResult = () => {
-   const rates = currenciesData[currency];
+    const rates = currenciesData[currency];
+
     setResult({
       sourceAmount: +amount,
       targetAmount: amount * rates,
@@ -28,49 +39,63 @@ const Form = () => {
     event.preventDefault();
     setAmount("");
   }
-  
+
   return (
-    <StyledForm onSubmit={onFormSubmit}>
+    <form onSubmit={onFormSubmit}>
       <Header>Kalkulator walutowy</Header>
       <Fieldset>
-        <Label>
-          <LabelText>
-            Kwota w zł*:
-          </LabelText>
-          <Input
-            placeholder="Wpisz kwotę w zł"
-            value={amount}
-            onChange={onSelectChange}
-            type="number"
-            required min="1"
-            step="1"
-          />
-        </Label>
-        <p>
-          <Label>
-            <LabelText>Waluta:</LabelText>
-            <Select
-              onChange={onSelectCurrency}
-              className="form__input"
-              name="currency"
-              value={currency} 
-            >
-              {Object.keys(currenciesData).map((currency) => (
-                <option
-                key={currency}
-                >
-                  {currency}
-                </option>
-              ))};
-            </Select>
-          </Label>
-        </p>
-        <Button>Przelicz</Button>
-        <p>
-          <Result result={result} />
-        </p>
+        {
+          loading ?
+            <ClipLoader
+              color={'#ffd700'}
+              loading={loading}
+              size={150}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+            :
+            <div>
+              <Label>
+                <LabelText>
+                  Kwota w zł*:
+                </LabelText>
+                <Input
+                  placeholder="Wpisz kwotę w zł"
+                  value={amount}
+                  onChange={onSelectChange}
+                  type="number"
+                  required min="1"
+                  step="1"
+                />
+              </Label>
+              <p>
+                <Label>
+                  <LabelText>Waluta:</LabelText>
+                  <Select
+                    onChange={onSelectCurrency}
+                    className="form__input"
+                    name="currency"
+                    value={currency}
+                  >
+                    {Object.keys(currenciesData).map((currency) => (
+                      <option
+                        key={currency}
+                      >
+                        {currency}
+                      </option>
+                    ))};
+                  </Select>
+                </Label>
+              </p>
+              <Button>Przelicz</Button>
+              <p>
+                <Result result={result} />
+              </p>
+            </div>
+        }
       </Fieldset>
-    </StyledForm>
+    </form>
   )
 };
+
 export default Form;
